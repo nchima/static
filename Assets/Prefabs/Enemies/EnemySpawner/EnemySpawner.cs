1f365183db9c502da3a252282b17d2176bdebcca
denniscarr <dcarr1@gmail.com>
2017-10-11 08:23:23 -0400

    Added very basic enemy spawner.

---
+++ Assets/Prefabs/Enemies/EnemySpawner/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : Enemy {
+
+	[SerializeField] GameObject enemyToSpawn;
+    [SerializeField] float timeBetweenSpawns;
+    float spawnTimer;
+
+
+    protected override void Update()
+    {
+        base.Update();
+
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= timeBetweenSpawns)
+        {
+            SpawnEnemy();
+            spawnTimer = 0;
+        }
+    }
+
+
+    void SpawnEnemy()
+    {
+        Vector3 newPosition = transform.position + new Vector3(Random.insideUnitCircle.x * 20f, 20f, Random.insideUnitCircle.y * 20f);
+
+        GameObject newEnemy = Instantiate(enemyToSpawn);
+        newEnemy.transform.position = newPosition;
+    }
+}
