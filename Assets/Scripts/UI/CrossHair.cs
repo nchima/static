aaaf3bfccc2146a3a1502a905fbcb15c998fcd64
denniscarr <dcarr1@gmail.com>
2018-07-09 17:51:02 -0400

    Changed crosshair visuals

--- Assets/Scripts/UI/CrossHair.cs
+++ Assets/Scripts/UI/CrossHair.cs
@@ -5,14 +5,15 @@ using DG.Tweening;
 
 public class CrossHair : MonoBehaviour {
 
-    public int circleSegments;
+    [SerializeField] FloatRange circleSegmentsRange = new FloatRange(8f, 12f);
+    int circleSegments { get { return Mathf.FloorToInt(GunValueManager.MapToFloatRangeInverted(circleSegmentsRange)); } }
     [SerializeField] float maxRadius = 1.25f;
     [SerializeField] float minRadius = 0.34f;
     float xradius;
     float yradius;
     LineRenderer line;
 
-    float shakeValueResting = 0.025f;
+    float shakeValueResting = 0.05f;
     public FloatRange shakeRange = new FloatRange(0.1f, 0.4f);
     float shakeValue;
 
@@ -42,8 +43,9 @@ public class CrossHair : MonoBehaviour {
     {
         xradius = MyMath.Map(GunValueManager.currentValue, -1f, 1f, maxRadius, minRadius);
         yradius = MyMath.Map(GunValueManager.currentValue, -1f, 1f, maxRadius, minRadius);
-        transform.Rotate(new Vector3(0f, Random.Range(-180f, 180f), 0f));
-        CircleDrawer.Draw(line, xradius, yradius, circleSegments, shakeValue);
+        CircleDrawer.DrawCrosshair(line, xradius, yradius, circleSegments, shakeValue);
+
+        //transform.Rotate(new Vector3(0f, Random.Range(-180f, 180f), 0f));
 
         // Lerp shake value back towards resting value
         shakeValue = Mathf.Lerp(shakeValue, shakeValueResting, 0.4f);
@@ -52,7 +54,7 @@ public class CrossHair : MonoBehaviour {
 
 
     void AdjustShakeValueForShotFired() {
-        shakeValue = MyMath.Map(GunValueManager.currentValue, 1f, -1f, shakeRange.min * 1.1f, shakeRange.max);
+        shakeValue = MyMath.Map(GunValueManager.currentValue, 1f, -1f, shakeRange.max * 1.1f, shakeRange.min);
     }
 
 
--- Assets/Scripts/Util/CircleDrawer.cs
+++ Assets/Scripts/Util/CircleDrawer.cs
@@ -23,4 +23,27 @@ public class CircleDrawer {
             angle += (360f / segments);
         }
     }
+
+
+    public static void DrawCrosshair(LineRenderer lineRenderer, float xRadius, float yRadius, int segments, float jitter) {
+        float x;
+        float y;
+        float z = 0f;
+
+        float angle = 20f;
+
+        if (lineRenderer.positionCount != segments + 1) { lineRenderer.positionCount = segments + 1; }
+
+        for (int i = 0; i < (segments + 1); i++) {
+            float xDeath = 0;
+            if (i % 2 == 0) { xDeath = GunValueManager.MapTo(0, xRadius); }
+            x = (Mathf.Sin(Mathf.Deg2Rad * angle) * (xRadius - xDeath * Random.Range(0.9f, 1.1f)) + Random.Range(-jitter, jitter));
+            y = 0f;
+            z = (Mathf.Cos(Mathf.Deg2Rad * angle) * (yRadius - xDeath * Random.Range(0.9f, 1.1f)) + Random.Range(-jitter, jitter));
+
+            lineRenderer.SetPosition(i, new Vector3(x, y, z));
+
+            angle += (360f / segments);
+        }
+    }
 }
