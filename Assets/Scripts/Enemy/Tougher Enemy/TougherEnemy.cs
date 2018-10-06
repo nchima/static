32d53b10a9de577d21fbdab48aab8d37ad47f3c7
denniscarr <dcarr1@gmail.com>
2018-10-05 23:33:51 -0400

    Added new enemy called 'Tougher Enemy'. Not implemented in any official levels yet.

---
+++ Assets/Scripts/Enemy/Tougher Enemy/TougherEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TougherEnemy : Enemy {
+
+    public float shootingDistance = 30f;
+    public float stoppingDistance = 10f;
+    public float distantMovementSpeed = 100f;
+    public float shootingMovementSpeed = 50f;
+    public float turningSpeed = 10f;
+    public GameObject shotPrefab;
+    public Transform shotOriginLeft;
+    public Transform shotOriginRight;
+
+    public Rigidbody m_Rigidbody { get { return GetComponent<Rigidbody>(); } }
+}
---
+++ Assets/Scripts/Enemy/Tougher Enemy/TougherEnemyState_Movement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TougherEnemyState_Movement : State {
+
+    float shotTimer = 0f;
+    float shotCooldown = 0.75f;
+
+    public override void Initialize(StateController stateController) {
+        base.Initialize(stateController);
+        TougherEnemy controller = stateController as TougherEnemy;
+
+        Vector3 destination = Services.playerTransform.position;
+        destination.y = controller.transform.position.y;
+        controller.m_NavMeshAgent.destination = destination;
+
+        shotTimer = 0f;
+    }
+
+    public override void Run(StateController stateController) {
+        base.Run(stateController);
+        TougherEnemy controller = stateController as TougherEnemy;
+
+        Vector3 destination = Services.playerTransform.position;
+        destination.y = controller.transform.position.y;
+        controller.m_NavMeshAgent.SetDestination(destination);
+
+        //Debug.DrawLine(controller.transform.position, controller.m_NavMeshAgent.des)
+
+        // Turn towards desired direction. forget this shit!! >:-[
+        //Vector3 desiredDirection = controller.m_NavMeshAgent.desiredVelocity.normalized;
+        //Vector3 newForward = controller.transform.forward;
+        //newForward = Vector3.RotateTowards(newForward, desiredDirection, controller.turningSpeed * Time.deltaTime, 0.0f);
+        //controller.transform.forward = newForward;
+
+        // Determine move speed
+        float distanceToPlayer = Vector3.Distance(controller.transform.position, Services.playerTransform.transform.position);
+        float moveSpeed = controller.distantMovementSpeed;
+        if (distanceToPlayer <= controller.stoppingDistance) { moveSpeed = 0f; }
+        else if (distanceToPlayer <= controller.shootingDistance) { moveSpeed = controller.shootingMovementSpeed; }
+
+        controller.m_NavMeshAgent.speed = moveSpeed;
+
+        // Move toward player
+        Vector3 newPosition = controller.transform.position;
+        newPosition += controller.transform.forward * moveSpeed * Time.deltaTime;
+        newPosition.y = 3.2f;
+        controller.m_Rigidbody.MovePosition(newPosition);
+
+        // Fire guns
+        shotTimer += Time.deltaTime;
+        if (shotTimer >= shotCooldown && (moveSpeed == controller.shootingMovementSpeed || moveSpeed == 0f)) {
+
+            NormalShot shot1 = Instantiate(controller.shotPrefab, controller.shotOriginLeft.position, Quaternion.identity).GetComponent<NormalShot>();
+            NormalShot shot2 = Instantiate(controller.shotPrefab, controller.shotOriginRight.position, Quaternion.identity).GetComponent<NormalShot>();
+
+            shot1.Direction = controller.transform.forward;
+            shot2.Direction = controller.transform.forward;
+
+            shotTimer = 0f;
+        }
+    }
+
+    public override void End(StateController stateController) {
+        base.End(stateController);
+    }
+}
